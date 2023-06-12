686ead761f06040bad5ca2999b139a9ae3c48073
Meer Khan <m33r.khan1996@gmail.com>
2023-06-12 20:24:06 +0500

    Code refactored

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,105 +1,34 @@
-import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
+import Landing1 from "../assets/Landing-1.webp";
+import Landing2 from "../assets/Landing-2.webp";
+import Landing3 from "../assets/Landing-3.webp";
+import Landing4 from "../assets/Landing-4.webp";
+import Carousel from "../components/custom/Carousel";
 import Welcome from "../components/custom/Welcome";
-
-const Carousel = ({ content = [] }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  let interval = useRef(null);
-
-  const showSlide = (index) => {
-    return activeIndex === index;
-  };
-  const nextSlide = useCallback(() => {
-    const nextIndex = activeIndex === content.length - 1 ? 0 : activeIndex + 1;
-    setIndex(nextIndex);
-  }, [activeIndex, content]);
-
-  const setIndex = (index) => {
-    setActiveIndex(index);
-  };
-
-  const clearInt = useCallback(() => {
-    if (interval.current) {
-      clearInterval(interval.current);
-    }
-  }, []);
-  const createInt = useCallback(() => {
-    clearInt();
-    interval.current = setInterval(() => {
-      nextSlide();
-    }, 5000);
-  }, [clearInt, nextSlide]);
-
-  useLayoutEffect(() => {
-    createInt();
-    () => {
-      clearInt();
-    };
-  }, [activeIndex, createInt, clearInt]);
-
-  const Slides = () => {
-    return content.map((cont, i) => {
-      let left;
-      let transform;
-      let zIndex;
-      if (i < activeIndex) {
-        left = `${(i - activeIndex) * 100}%`;
-        transform = "";
-        zIndex = -1;
-      } else if (i > activeIndex) {
-        left = `${(i - activeIndex) * 100}%`;
-        transform = "";
-        zIndex = -1;
-      } else {
-        left = `${i - activeIndex + 50}%`;
-        transform = "translate(-50%, 0%)";
-        zIndex = 1;
-      }
-      return (
-        <div
-          className="slide"
-          style={{
-            // backgroundImage: `url(${cont.image}), linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(254,254,255,0) 100%)`,
-            left,
-            transform,
-            zIndex,
-          }}
-          key={`${cont.title}-${i}`}
-        >
-          <img className="slide-background" src={cont.image} />
-
-          <h4
-            className="display1 font-family-2 t-cyan shadow-cyan"
-            style={{ textAlign: "center" }}
-          >
-            {cont.title}
-          </h4>
-        </div>
-      );
-    });
-  };
-
-  const Pagination = () => {
-    return content.map((cont, i) => {
-      return (
-        <div className="pg_item " key={i} onClick={() => setActiveIndex(i)}>
-          {showSlide(i) && <div className="progress" />}
-        </div>
-      );
-    });
-  };
+const HomePage = () => {
+  const content = [
+    {
+      image: Landing1,
+      title: "Total Energy Power Utilities",
+    },
+    {
+      image: Landing2,
+      title: "Gas to the business you run",
+    },
+    {
+      image: Landing3,
+      title: "Best Electricity Prices for your business",
+    },
+    {
+      image: Landing4,
+      title: "Business Landline & Broadband",
+    },
+  ];
   return (
-    <div className="carousel d-flex-column">
-      <div className="carousel" key="carousel" id="home">
-        {Slides()}
-
-        <div className="slider_pagination_container flex-wrap">
-          <Pagination />
-        </div>
-      </div>
-
+    <div>
+      <Carousel content={content} />
       <Welcome />
     </div>
   );
 };
 
-export default Carousel;
+export default HomePage;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,36 +1,16 @@
-import Carousel from "src/pages/HomePage";
+
 import About from "src/pages/AboutusPage";
 import ContactUs from "src/pages/ContactUsPage";
 import Faqs from "src/pages/FaqsPage";
-import Landing1 from "../assets/Landing-1.jpg";
-import Landing2 from "../assets/Landing-2.jpg";
-import Landing3 from "../assets/Landing-3.jpg";
-import Landing4 from "../assets/Landing-4.jpg";
 import Complaints from "src/pages/ComplaintsPage";
 import { Route, Routes } from "react-router-dom";
+import HomePage from "src/pages/HomePage";
 const Landing = () => {
-  const content = [
-    {
-      image: Landing1,
-      title: "Total Energy Power Utilities",
-    },
-    {
-      image: Landing2,
-      title: "Gas to the business you run",
-    },
-    {
-      image: Landing3,
-      title: "Best Electricity Prices for your business",
-    },
-    {
-      image: Landing4,
-      title: "Business Landline & Broadband",
-    },
-  ];
+ 
   return (
     <div className="landing_page">
       <Routes>
-        <Route path="/" element={<Carousel content={content} />}></Route>
+        <Route path="/" element={<HomePage/>}></Route>
         <Route path="about-us" element={<About />}></Route>
         <Route path="complaints" element={<Complaints />}></Route>
         <Route path="faqs" element={<Faqs />}></Route>
